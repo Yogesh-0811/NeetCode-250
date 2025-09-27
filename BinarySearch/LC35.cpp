31d082540512ee2868b3a9c3ce220d901ac3d82f
Yogesh <yogeshpatil0811d@gmail.com>
2025-09-27 19:49:29 +0530

    Leetcode 35

---
+++ BinarySearch/LC35.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int low, int high, int target) {
+        if (low > high) return low; 
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] > target) return binarySearch(nums, low, mid - 1, target);
+        else return binarySearch(nums, mid + 1, high, target);
+    }
+
+    int searchInsert(vector<int>& nums, int target) {
+        return binarySearch(nums, 0, nums.size() - 1, target);
+    }
+};
+
+int main() {
+    int n, target;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter sorted array elements: ";
+    for (int i = 0; i < n; i++) cin >> nums[i];
+
+    cout << "Enter target element: ";
+    cin >> target;
+
+    Solution sol;
+    int result = sol.searchInsert(nums, target);
+
+    cout << "Target position: " << result << endl;
+
+    return 0;
+}
