e49c037f5f13f4b5a680441c3950532b5a4a4a18
Yogesh <yogeshpatil0811d@gmail.com>
2025-10-05 18:05:13 +0530

    Leetcode 81

---
+++ BinarySearch/LC81.cpp
@@ -0,0 +1,74 @@
+// There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+// Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+// Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+// You must decrease the overall operation steps as much as possible.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 0
+// Output: true
+// Example 2:
+
+// Input: nums = [2,5,6,0,0,1,2], target = 3
+// Output: false
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool search(vector<int>&nums, int target){
+    int n=nums.size();
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target){
+            return true;
+        }
+        if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+            low++;
+            high--;
+            continue;
+        }
+        if(nums[mid]>=nums[low]){
+            if(target>=nums[low]&&target<nums[mid]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }else{
+            if(target>nums[mid]&&target<=nums[high]){
+                low=mid+1;
+            }else{
+                low=mid-1;
+            }
+        }
+    }
+    return false;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+
+    vector<int>nums(n);
+    cout<<"Enter the elements in arr: ";
+    for(int &num:nums) cin>>num;
+
+    int target;
+    cout<<"Enter the target element: ";
+    cin>>target;
+
+    if(search(nums,target)){
+        cout<<"Target found"<<endl;
+    }else{
+        cout<<"Target not found"<<endl;
+    }
+
+    return 0;
+}
